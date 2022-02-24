c4333fa4414d350906a8b020dee9b11d2af8bc0f
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-02-24 22:15:26 +0200

    ++

---
+++ Inheritance-Exercise/Person/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        public string name;
+
+        public int age;
+
+        public string Name{ get => name; set => name = value; }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 0 )
+                {
+                    throw new Exception();
+                }
+
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                this.Name,
+                this.Age));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
