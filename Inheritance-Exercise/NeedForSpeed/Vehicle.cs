c68fb4575da53f5f49692841d892b0d2a3d101f1
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-02-25 22:07:11 +0200

    2 more to go!

---
+++ Inheritance-Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+
+            Fuel = fuel;
+
+        }
+
+        public double DefaultFuelConsumption => 1.25;
+
+        private double fuelConsumption;
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public double Fuel { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers) => Fuel -= FuelConsumption * kilometers;
+        
+    }
+}
