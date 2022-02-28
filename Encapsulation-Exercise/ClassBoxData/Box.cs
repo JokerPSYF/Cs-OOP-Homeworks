51321c46ca84938f1e8bcf479a7f169bc6858352
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-02-28 22:06:04 +0200

    ++

---
+++ Encapsulation-Exercise/ClassBoxData/Box.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        //Length - double, should not be zero or negative number
+        //Width - double, should not be zero or negative number
+        //Height - double, should not be zero or negative number
+
+        private double length;
+        private double width;
+        private double height;
+
+        public double Length
+        {
+            get => length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                else
+                {
+                    length = value;
+                }
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                else
+                {
+                    width = value;
+                }
+            }
+        }
+
+        public double Height
+        {
+            get => height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                else
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double SurfaceArea() => (2 * length * width) + (2 * length * height) + (2 * width * height);
+
+        public double LateralSurfaceArea() => (2 * length * height) + (2 * width * height);
+
+        public double Volume() => length * height * width;
+
+
+    }
+}
