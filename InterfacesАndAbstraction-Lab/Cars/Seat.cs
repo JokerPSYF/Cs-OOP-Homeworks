89193afb4e0188608e94feba7c6bd0018f77d395
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-03-04 16:24:52 +0200

    complete it

---
+++ InterfacesАndAbstraction-Lab/Cars/Seat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        private string model;
+
+        private string color;
+
+        public string Model { get => model; set => model = value; }
+        public string Color { get => color; set => color = value; }
+
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Start() => "Engine start";
+
+
+        public string Stop() => "Breaaak!";
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.color} Seat {this.model}");
+            sb.AppendLine(Start());
+            sb.AppendLine(Stop());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ InterfacesАndAbstraction-Lab/Cars/Tesla.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        private string model;
+
+        private string color;
+
+        private int battery;
+
+        public Tesla(string model, string color, int batteries )
+        {
+            Model = model;
+            Color = color;
+            Battery = batteries;
+        }
+
+        public string Model { get => model; set => model = value; }
+        public string Color { get => color; set => color = value; }
+
+        public string Start() => "Engine start";
+
+        public string Stop() => "Breaaak!";
+
+        public int Battery { get => battery; set => battery = value; }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.AppendLine($"{this.color} Tesla {this.model} with {battery} Batteries");
+
+            builder.AppendLine(Start());
+
+            builder.AppendLine(Stop());
+
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
