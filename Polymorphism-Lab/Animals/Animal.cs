def9f34c41d2a55d8d5b52412fc7d4622d6979ad
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-03-06 00:34:18 +0200

    Polymorphism Lab complete at 6.3.22

---
+++ Polymorphism-Lab/Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; private set; }
+
+        public string FavouriteFood { get; private set; }
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+         
+        public virtual string ExplainSelf()
+        {
+            StringBuilder builder = new StringBuilder($"I am {this.Name} and my fovourite food is {this.FavouriteFood}");
+            return builder.ToString();
+        }
+    }
+}
---
+++ Polymorphism-Lab/Operations/MathOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        //Add(int, int) : int
+        //Add(double, double, double) : double
+        //Add(decimal, decimal, decimal) : decimal
+
+       public int Add(int a, int b) => a + b;
+
+        public double Add(double a, double b, double c) => a + b + c;
+
+        public  decimal Add(decimal a, decimal b, decimal c) => a + b + c;
+    }
+}
