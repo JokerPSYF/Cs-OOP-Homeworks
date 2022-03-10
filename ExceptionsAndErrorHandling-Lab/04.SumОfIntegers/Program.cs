03e4db78d639b77f7af7fa64d8770b311a63aa92
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-03-11 00:03:20 +0200

    ++

---
+++ ExceptionsAndErrorHandling-Lab/04.SumОfIntegers/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _04.SumОfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+
+            string[] input = Console.ReadLine().Split();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                try
+                {
+                    int result;
+                    if (int.TryParse(input[i], out result))
+                    {
+                        sum += result;
+                    }
+                    else
+                    {
+                        long lomgNum;
+                        if (long.TryParse(input[i], out lomgNum))
+                        {
+                            throw new OverflowException();
+                        }
+                        else
+                        {
+                            throw new FormatException();
+                        }
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"The element '{input[i]}' is in wrong format!");
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine($"The element '{input[i]}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{input[i]}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
