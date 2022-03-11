ff960a7930cddb3b08591fbd0e38c04f47b6acc0
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-03-11 21:37:34 +0200

    complete

--- ExceptionsAndErrorHandling-Lab/06.MoneyTransactions/Program.cs
+++ ExceptionsAndErrorHandling-Lab/06.MoneyTransactions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace _06.MoneyTransactions
@@ -7,16 +8,64 @@ namespace _06.MoneyTransactions
     {
         static void Main(string[] args)
         {
-            int[] nums = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            Dictionary<int, double> balances = new Dictionary<int, double>();
 
-            string[] array = {"lazy", "dog", "jumps", "Over",
-                "the", "Lazy", "fox" };
+            string[] firstInput = Console.ReadLine()
+                .Split(new char[] { '-', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
 
-            Console.WriteLine(string.Join(", ", nums.Select(x => x.ToString()).ToArray(), 3, 5));
-            Console.WriteLine(string.Join(", ", array, 1, 3));
+            for (int i = 0; i < firstInput.Length; i += 2)
+            {
+                balances.Add(int.Parse(firstInput[i]), double.Parse(firstInput[i + 1]));
+            }
 
-            var result = string.Join(", ", nums.Select(x => x.ToString()).ToArray());
-            Console.WriteLine(result);
+            string[] cmd = Console.ReadLine().Split();
+
+            while (cmd[0] != "End")
+            {
+                string command = cmd[0];
+                int balance = int.Parse(cmd[1]);
+                double sum = double.Parse(cmd[2]);
+                try
+                {
+                    switch (command)
+                    {
+                        case "Deposit":
+                            if (balances.ContainsKey(balance)) balances[balance] += sum;
+                            else throw new ArgumentException("Invalid account!");
+                            break;
+
+                        case "Withdraw":
+                            if (balances.ContainsKey(balance))
+                            {
+                                if (sum <= balances[balance])
+                                {
+                                    balances[balance] -= sum;
+                                }
+                                else
+                                {
+                                    throw new ArgumentException("Insufficient balance!");
+                                }
+                            }
+                            else throw new ArgumentException("Invalid account!");
+                            break;
+
+                        default: throw new ArgumentException("Invalid command!");
+                    }
+
+                    Console.WriteLine($"Account {balance} has new balance: {balances[balance]:f2}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Enter another command");
+                }
+
+                cmd = Console.ReadLine().Split();
+            }
         }
     }
 }
