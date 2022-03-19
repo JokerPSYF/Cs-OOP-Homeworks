3f395924eda3734572bc4a6aed8aa721f374c49e
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-03-19 20:50:00 +0200

    command pattern  complete it

---
+++ ReflectionAndAttributes-Exercise/CommandPattern/Model/Engine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            try
+            {
+                while (true)
+                {
+                    string args = Console.ReadLine();
+
+                    string result = this.commandInterpreter.Read(args);
+
+                    Console.WriteLine(result);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
