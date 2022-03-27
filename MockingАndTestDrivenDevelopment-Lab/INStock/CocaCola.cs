5c0bc184ff3b1ab393df1d1fb4d150430d12fc7a
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-03-27 22:42:07 +0300

    INStock started

---
+++ MockingАndTestDrivenDevelopment-Lab/INStock/CocaCola.cs
@@ -0,0 +1,16 @@
+﻿using INStock.Contracts;
+
+namespace INStock
+{
+    public class CocaCola : IProduct
+    {
+        public int CompareTo(IProduct other)
+        {
+            return Price.CompareTo(other.Price);
+        }
+
+        public string Label { get => "Soda"; }
+        public decimal Price { get => 2.7m; }
+        public int Quantity { get => 2; }
+    }
+}
---
+++ MockingАndTestDrivenDevelopment-Lab/INStock/Kaufland.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using INStock.Contracts;
+
+namespace INStock
+{
+    public class Kaufland : IProductStock
+    {
+        private List<IProduct> products = new List<IProduct>();
+
+        public IEnumerator<IProduct> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int Count { get; }
+
+        public IProduct this[int index]
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
+
+        public bool Contains(IProduct product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Add(IProduct product)
+        {
+            products.Add(product);
+        }
+
+        public bool Remove(IProduct product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IProduct Find(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IProduct FindByLabel(string label)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IProduct FindMostExpensiveProduct()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<IProduct> FindAllInRange(double lo, double hi)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<IProduct> FindAllByPrice(double price)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<IProduct> FindAllByQuantity(int quantity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
