e20801ea709fc6aa48c90c5ed34836120f196173
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-03-30 21:42:47 +0300

    Vessel class implemented

---
+++ CsOOPRetakeExam-20Dec2021/01. Structure_Skeleton/NavalVessels-Skeleton/NavalVessels/Models/Battleship.cs
@@ -0,0 +1,20 @@
+﻿using NavalVessels.Models.Contracts;
+
+namespace NavalVessels.Models
+{
+    public class Battleship : Vessel, IBattleship
+    {
+        public Battleship(string name, double mainWeaponCaliber, double speed, double armorThickness) 
+            : base(name, mainWeaponCaliber, speed, armorThickness)
+        {
+            SonarMode = false;
+            ArmorThickness = 300;
+        }
+
+        public bool SonarMode { get; }
+        public void ToggleSonarMode()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CsOOPRetakeExam-20Dec2021/01. Structure_Skeleton/NavalVessels-Skeleton/NavalVessels/Models/Vessel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using NavalVessels.Models.Contracts;
+using NavalVessels.Utilities.Messages;
+
+namespace NavalVessels.Models
+{
+    public class Vessel : IVessel
+    {
+        private string name;
+        private ICaptain captain;
+        private double defaultArmorThikness;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(ExceptionMessages.InvalidVesselName);
+                }
+            }
+        }
+
+        public ICaptain Captain
+        {
+            get
+            {
+                return captain;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new NullReferenceException(ExceptionMessages.InvalidCaptainToVessel);
+                }
+            }
+        }
+
+        public double ArmorThickness { get; set; }
+        public double MainWeaponCaliber { get; }
+        public double Speed { get; }
+        public ICollection<string> Targets { get; }
+
+        public Vessel(string name, double mainWeaponCaliber, double speed, double armorThickness)
+        {
+            Name = name;
+            ArmorThickness = armorThickness;
+            defaultArmorThikness = armorThickness;
+            MainWeaponCaliber = mainWeaponCaliber;
+            Speed = speed;
+        }
+
+        public void Attack(IVessel target)
+        {
+            if (target == null) throw new NullReferenceException(ExceptionMessages.InvalidTarget);
+
+            target.ArmorThickness -= MainWeaponCaliber;
+
+            if (target.MainWeaponCaliber < 0)
+            {
+                target.ArmorThickness = 0;
+
+                Targets.Add(target.Name);
+            }
+        }
+
+        public void RepairVessel()
+        {
+            if (this.GetType() is IBattleship)
+            {
+                ArmorThickness = 300;
+            }
+            else if (this.GetType() is ISubmarine)
+            {
+                ArmorThickness = 300;
+            }
+            else
+            {
+                ArmorThickness = 200;
+            }
+        }
+
+        public string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"- {Name}");
+            sb.AppendLine($"*Type: {GetType().Name}");
+            sb.AppendLine($"*Armor thickness: {ArmorThickness}");
+            sb.AppendLine($"*Main weapon caliber: {MainWeaponCaliber}");
+            sb.AppendLine($"*Speed: {Speed} knots");
+            sb.AppendLine($"- {Name}");
+            if (Targets.Count == 0) sb.AppendLine("*Targets: None");
+            else sb.AppendLine($"*Targets: {string.Join(", ", Targets)}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
