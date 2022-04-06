1b6d191e1391bdc5bee580fe2ad1180c9afc0567
Todor Dimitrov Vasilev <todorvasilev005@gmail.com>
2022-04-07 00:08:14 +0300

    ++

--- CsOOPRetakeExam-19December2020-WarCroft/FestivalManager-UnitTest/FestivalManager.Tests/StageTests.cs
+++ CsOOPRetakeExam-19December2020-WarCroft/FestivalManager-UnitTest/FestivalManager.Tests/StageTests.cs
@@ -1,6 +1,9 @@
 // Use this file for your unit tests.
 // When you are ready to submit, REMOVE all using statements to Festival Manager (entities/controllers/etc)
 // Test ONLY the Stage class. 
+
+using FestivalManager.Entities;
+
 namespace FestivalManager.Tests
 {
     using NUnit.Framework;
@@ -10,9 +13,25 @@ namespace FestivalManager.Tests
 	public class StageTests
     {
 		[Test]
-	    public void test1()
-	    {
-		}
+	    public void CtorTest()
+        {
+            Stage stage = new Stage();
+            Assert.AreEqual(0,stage.Performers.Count);
+        }
+        
+        [Test]
+	    public void AddPerfomereThrowException()
+        {
+            Stage stage = new Stage();
+            Assert.AreEqual(0,stage.Performers.Count);
+        }
+        
+        [Test]
+	    public void CtorsTest()
+        {
+            Stage stage = new Stage();
+            Assert.AreEqual(0,stage.Performers.Count);
+        }
 
 	}
 }
